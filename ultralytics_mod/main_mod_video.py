a901d36f55a3b2d0da7e732f62de89967e6d1467
AlexanderAkhtar <115800318+AlexanderAkhtar@users.noreply.github.com>
2024-05-13 11:45:00 +0200

    Update main_mod_video.py

--- ultralytics_mod/main_mod_video.py
+++ ultralytics_mod/main_mod_video.py
@@ -9,12 +9,6 @@ import supervision as sv
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description= "YOLOv8 live")
-    parser.add_argument(
-        "--webcam-resolution",
-        default=[1920, 1080],
-        nargs=2,
-        type=int
-    )
   parser.add_argument(
         '--video', 
         help='Path to video file', ##might need to be help not file_name
@@ -24,24 +18,18 @@ def parse_arguments() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
-
 def main():
     args = parse_arguments()
     if args.video == '' and args.webcam == '':
-        raise ValueError('Either --video or --webcam has to be provided')
-    if args.webcam != ''
-        frame_width, frame_height = args.webcam_resolution
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+        raise ValueError('--video has to be provided')
     if args.video != '':
         frame_provider = VideoReader(args.video)
         is_video = True
     base_height = args.height_size
     fx = args.fx
     model = YOLO("yolov8l.pt")
-
     c = time.time()
+    a = 1
     with open("result.csv", mode="w", newline='') as csvfile:
         field = ["frame nummer", "x1=", "y1=","x2=", "y2=", "time="]
         writer = csv.DictWriter(csvfile, fieldnames=field)
@@ -53,10 +41,7 @@ def main():
             text_scale=1
         )
 
-        while True:
-            ret, frame = cap.read()
-            a = a+1
-
+        for frame in frame_provider:
             result = model(frame)[0]
             detections = sv.Detections.from_ultralytics(result)
             for i in range(len(detections)):
@@ -64,10 +49,9 @@ def main():
                 d = time.time()
                 writer.writerow({"frame nummer": a, "x1=": x1, "y1=": y1,"x2=": x2, "y2=": y2, "time=": d-c})
 
+            a = a+1
             frame = box_annotator.annotate(scene=frame, detections=detections)
-
             cv2.imshow("yolov8", frame)
-
             if (cv2.waitKey(30) == 27):
                 break
 
