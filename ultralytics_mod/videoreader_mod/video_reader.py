9822087dcf1be4438c5467e408ef3cee123b1cb2
AlexanderAkhtar <115800318+AlexanderAkhtar@users.noreply.github.com>
2024-05-13 10:09:13 +0200

    Create video_reader.py

---
+++ ultralytics_mod/videoreader_mod/video_reader.py
@@ -0,0 +1,21 @@
+class VideoReader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        try:  # OpenCV needs int to read from webcam
+            self.file_name = int(file_name)
+        except ValueError:
+            pass
+
+    def __iter__(self):
+        self.cap = cv2.VideoCapture(self.file_name)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        if not self.cap.isOpened():
+            raise IOError('Video {} cannot be opened'.format(self.file_name))
+        return self
+
+    def __next__(self):
+        was_read, img = self.cap.read()
+        if not was_read:
+            raise StopIteration
+        return img
